3f364e4e151ef7704becd8b2855c4c9ef6d138d0
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-01-06 23:20:01 -0400

    Se crearon rutas y alias para facilitar los imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      },
+    },
   },
 });
